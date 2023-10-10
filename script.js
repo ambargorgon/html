74d19f0fe9cb0d0e4add1953a8ffeb6b4e6abbc4
Ambar Gorgon <ambargorgon@gmail.com>
2023-10-10 14:48:01 -0300

    fix commit

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 currentUrl = window.location.href
 if (currentUrl.includes("index.html")) {
     // Navegar a Sintesis
-    let botonSintesis = document.querySelector("#button_sintesis")
+    let botonSintesis = document.querySelector(".button_sintesis")
 
     botonSintesis.addEventListener("click", () => {
         window.location.href = "sintesis.html"
     })
+
 } else if (currentUrl.includes("sintesis.html")) {
     //Navegar a conclusion
-    let botonConclusion = document.querySelector("#button_conclusion")
-
-    botonConclusion.addEventListener("click", () => {
-        window.location.href = "conclusion.html"
-    })
-
+    let botonConclusion = document.querySelectorAll(".button_conclusion")
+    console.log(botonConclusion)
+    for (let boton of botonConclusion){
+        boton.addEventListener("click", () => {
+            window.location.href = "conclusion.html"
+        })
+    }
 } else {
-    let botonInicio = document.querySelector("#button_toHome")
+    //Navegar a home
+    let botonInicio = document.querySelector(".button_toHome")
     botonInicio.addEventListener("click", () => {
         window.location.href = "index.html"
     })
+
 }
 
 // Navegar atras
